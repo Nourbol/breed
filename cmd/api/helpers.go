d916947e542489507b710efab253665e23b4dba9
Zholdas <211751@astanait.edu.kz>
2023-02-22 17:26:08 +0600

    Были не правильно написаны буквы соре добавлен helpers.goeeeeeee

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -8,6 +8,8 @@ import (
 	"strconv"
 )
 
+type envelope map[string]any
+
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
@@ -17,9 +19,7 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-type envelope map[string]any
-
-func (app *applicaton) writeJSON(w http.ResponseWrite, status int, data envelope, headers http.Header) error {
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
