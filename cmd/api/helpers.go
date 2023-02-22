9d10009d81d4dd61ae5b49a19bb8694ed07d891e
Zholdas <211751@astanait.edu.kz>
2023-02-22 15:54:17 +0600

    создан errors.go изменены api\breeds.go, healthcheck.go, helpers.go, routes.go
    присудствуют ошибки в файлах api\breeds.go, healthcheck.go, helpers.go которые я не понял

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
@@ -14,4 +15,25 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 		return 0, errors.New("invalid id parameter")
 	}
 	return id, nil
+}
+
+type envelope map[string]any
+
+func (app *applicaton) writeJSON(w http.ResponseWrite, status int, data envelope, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
