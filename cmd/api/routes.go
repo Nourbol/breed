ae78a8fc64d100de3c8f1791df94019ef12e0784
Zholdas <211751@astanait.edu.kz>
2023-02-23 08:34:23 +0600

    добавлены файлы jsonlog.go and middleware.go изменены errors.go, main.go,routes.go присудствуют ошибки в main.go, исправить не получилось

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
@@ -19,5 +19,5 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodPatch, "/v1/breeds/:id", app.updateBreedHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/breeds/:id", app.deleteBreedHandler)
 
-	return router
+	return app.recoverPanic(router)
 }
